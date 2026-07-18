59aded628e0941da9a99b846b1b95276c9b3153b
ntjoann12 <novejoanofc@gmail.com>
2026-07-17 20:58:16 -0600

    Agregando el contexto de DataBase

--- Steam2Api/Program.cs
+++ Steam2Api/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Steam2Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext> (options => 
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
