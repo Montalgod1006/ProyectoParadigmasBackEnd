2e678d02323751bea6cad1e21003ae4f0cff39a6
Montalban <amontalban69@gmail.com>
2026-08-02 16:23:21 -0600

    Agregando cositas a la base de datos

--- Steam2Api/obj/Debug/net10.0/Steam2Api.AssemblyInfo.cs
+++ Steam2Api/obj/Debug/net10.0/Steam2Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Steam2Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+199a754edebeb58b51a40c82fabdafb207e79b92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27d7b64195876e1ab3f01d42186ababf319e5b4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Steam2Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Steam2Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
